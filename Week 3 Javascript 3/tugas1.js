1e190510e1cbc5cd0d4b53f858cc568a92375be8
Muhamad Islahuddin <muhislah>
2022-04-21 08:49:20 +0700

    selesai tugas week 3 javascript 3

--- Week 3 Javascript 3/tugas1.js
+++ Week 3 Javascript 3/tugas1.js
@@ -4,31 +4,31 @@ const cekHariKerja = (day) => {
             const dataDay = ['senin','selasa','rabu','kamis','jumat'];
             let cek = dataDay.find(item => {
                 return item === day
-            })
-            if (cek){
+            }) // cek return first data that contain condition
+            if (cek){ // execute when not null / not undefined
                 resolve(cek)
-            }else{
-                reject(new Error('Hari ini bukan Hari Kerja'))
+            }else{ // execute when undefined
+                reject(new Error(`Hari ${day} bukan Hari Kerja`))
             }
         }, 3000)
     })
 }
 
 // menggunakan then dan catch
-// cekHariKerja('jumat')
-// .then(data => {
-//     console.log(`${data} adalah hari kerja`);
-// })
-// .catch(data =>
-//     console.log(data))
+cekHariKerja('minggu')
+.then(data => {
+    console.log(`${data} adalah hari kerja`);
+})
+.catch(data =>
+    console.log(data))
 
 // menggunakan try dan catch
-const result = async () => {
-    try {
-        const data = await cekHariKerja('jumat')
-        console.log(`${data} adalah hari kerja`)
-    }catch(err){
-        console.log(err)
-    }
-}
-result()
+// const result = async () => {
+//     try {
+//         const data = await cekHariKerja('jumat')
+//         console.log(`${data} adalah hari kerja`)
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
+// result()
